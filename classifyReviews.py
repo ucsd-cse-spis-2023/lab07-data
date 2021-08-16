46793d35a95f75ecbe233b535742713d0f6db72e
Niema Moshiri <niemamoshiri@gmail.com>
2021-08-16 14:07:54 -0700

    Change camel-case to snake-case

--- classifyReviews.py
+++ classifyReviews.py
@@ -14,60 +14,60 @@ def format_sentence(sent):
     tokens = nltk.word_tokenize(sent)
     return({word: True for word in tokens})
 
-def getReviews(data, rating):
+def get_reviews(data, rating):
     ''' Return the reviews from the rows in the data set with the
         given rating '''
     rows = data['Rating']==rating
     return list(data.loc[rows, 'Review'])
 
 
-def splitTrainTest(data, trainProp):
-    ''' input: A list of data, trainProp is a number between 0 and 1
+def split_train_test(data, train_prop):
+    ''' input: A list of data, train_prop is a number between 0 and 1
               specifying the proportion of data in the training set.
         output: A tuple of two lists, (training, testing)
     '''
     # TODO: You will write this function, and change the return value
     return ([], [])
 
-def formatForClassifier(dataList, label):
+def format_for_classifier(data_list, label):
     ''' input: A list of documents represented as text strings
                The label of the text strings.
-        output: a list with one element for each doc in dataList,
+        output: a list with one element for each doc in data_list,
                 where each entry is a list of two elements:
                 [format_sentence(doc), label]
     '''
     # TODO: Write this function, change the return value
     return []
 
-def classifyReviews():
+def classify_reviews():
     ''' Perform sentiment classification on movie reviews ''' 
     # Read the data from the file
-    data = pd.read_csv("data/movieReviews.csv")
+    data = pd.read_csv("data/movie_reviews.csv")
 
     # get the text of the positive and negative reviews only.
     # positive and negative will be lists of strings
     # For now we use only very positive and very negative reviews.
-    positive = getReviews(data, 4)
-    negative = getReviews(data, 0)
+    positive = get_reviews(data, 4)
+    negative = get_reviews(data, 0)
 
     # Split each data set into training and testing sets.
-    # You have to write the function splitTrainTest
-    (posTrainText, posTestText) = splitTrainTest(positive, 0.8)
-    (negTrainText, negTestText) = splitTrainTest(negative, 0.8)
+    # You have to write the function split_train_test
+    (pos_train_text, pos_test_text) = split_train_test(positive, 0.8)
+    (neg_train_text, neg_test_text) = split_train_test(negative, 0.8)
 
     # Format the data to be passed to the classifier.
-    # You have to write the formatForClassifer function
-    posTrain = formatForClassifier(posTrainText, 'pos')
-    negTrain = formatForClassifier(negTrainText, 'neg')
+    # You have to write the format_for_classifier function
+    pos_train = format_for_classifier(pos_train_text, 'pos')
+    neg_train = format_for_classifier(neg_train_text, 'neg')
 
     # Create the training set by appending the pos and neg training examples
-    training = posTrain + negTrain
+    training = pos_train + neg_train
 
     # Format the testing data for use with the classifier
-    posTest = formatForClassifier(posTestText, 'pos')
-    negTest = formatForClassifier(negTestText, 'neg')
+    pos_test = format_for_classifier(pos_test_text, 'pos')
+    neg_test = format_for_classifier(neg_test_text, 'neg')
     # Create the test set
-    test = posTest + negTest
+    test = pos_test + neg_test
 
 
     # Train a Naive Bayes Classifier
@@ -90,4 +90,4 @@ def classifyReviews():
 
 
 if __name__ == "__main__":
-    classifyReviews()
+    classify_reviews()
